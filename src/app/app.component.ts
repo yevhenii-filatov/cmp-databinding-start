a2a46d6f68a2bd9ab4e3c5f510fc4f55c9d34836
Yevhenii Filatov <yevhenii.filatov@extrawest.com>
2021-08-28 17:35:08 +0300

    Up to lecture 80

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,4 +24,11 @@ export class AppComponent {
     });
   }
 
+  onChangeFirst() {
+    this.serverElements[0].name = 'Changed';
+  }
+
+  onDestroyFirst() {
+    this.serverElements.splice(0, 1);
+  }
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,17 +1,76 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  ContentChild,
+  DoCheck,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
   @Input('serverElement') element: { name: string, type: string, content: string };
+  @Input() name: string;
+  @ViewChild('heading', {static: true}) header: ElementRef;
+  @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef;
 
   constructor() {
+    console.log('constructor');
   }
 
   ngOnInit(): void {
+    console.log('on init');
+    console.log('text content = ' + this.header.nativeElement.textContent);
+    console.log('text content of paragraph = ' + this.paragraph.nativeElement.textContent);
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('on changes');
+    console.log(changes);
+  }
+
+  ngDoCheck(): void {
+    console.log('do check');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('after content init');
+    console.log('text content of paragraph = ' + this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('after content checked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('after view init');
+    console.log('text content = ' + this.header.nativeElement.textContent);
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('after view checked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('on destroy');
+  }
 }
